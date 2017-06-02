259ff53c733cb1c474e8a86e0ec6b2347c32035a
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-02 14:22:02 -0500

    refactor(rappid-backend-core/mapper): remove simple mapper pattern using Dozer and create better JSE 8 mapper using MapStruct

--- rappid-backend-core/src/main/java/org/com/rappid/mapper/ChefMapper.java
+++ rappid-backend-core/src/main/java/org/com/rappid/mapper/ChefMapper.java
@@ -2,15 +2,16 @@ package org.com.rappid.mapper;
 
 import org.com.rappid.domain.Chef;
 import org.com.rappid.entity.ChefEntity;
-import org.com.rappid.mapper.jpa.impl.GenericMapperImpl;
-import org.com.rappid.stereotype.Mapper;
-
-import javax.ejb.Singleton;
+import org.com.rappid.mapper.jpa.GenericMapper;
+import org.mapstruct.Mapper;
 
 /**
  * Created by PINA on 31/05/2017.
  */
-@Mapper
-@Singleton
-public class ChefMapper extends GenericMapperImpl<Chef, ChefEntity> {
+@Mapper(componentModel = "cdi")
+public interface ChefMapper extends GenericMapper<Chef, ChefEntity> {
+
+    Chef map(final ChefEntity chefEntity);
+
+    ChefEntity map(final Chef chef);
 }
--- rappid-backend-core/src/main/java/org/com/rappid/mapper/jpa/GenericMapper.java
+++ rappid-backend-core/src/main/java/org/com/rappid/mapper/jpa/GenericMapper.java
@@ -1,25 +1,57 @@
 package org.com.rappid.mapper.jpa;
 
+import javax.ejb.AsyncResult;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 /**
  * Created by PINA on 25/05/2017.
  */
+
 public interface GenericMapper<F, T extends Serializable> {
 
     T map(final F from);
 
     F map(final T from);
 
-    Future<F> map(final Future<T> from);
+    default Future<F> map(final Future<T> from) {
+        return Optional.of(from).map(f -> {
+            try {
+                return new AsyncResult<>(this.map(f.get()));
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        }).orElse(null);
+    }
 
-    List<T> mapList(final List<F> from);
+    default List<T> mapList(final List<F> from) {
+        return Optional.of(from).orElse(null).stream().map(this::map).collect(Collectors.toList());
+    }
 
-    List<F> mapListReverse(final List<T> from);
+    default Future<List<T>> mapList(final Future<List<F>> from) {
+        return Optional.of(from).map(f -> {
+            try {
+                return new AsyncResult<>(this.mapList(f.get()));
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        }).orElse(null);
+    }
 
-    Future<List<T>> mapList(final Future<List<F>> from);
+    default List<F> mapListReverse(final List<T> from) {
+        return Optional.of(from).orElse(null).stream().map(this::map).collect(Collectors.toList());
+    }
 
-    Future<List<F>> mapListReverse(final Future<List<T>> from);
+    default Future<List<F>> mapListReverse(final Future<List<T>> from) {
+        return Optional.of(from).map(f -> {
+            try {
+                return new AsyncResult<>(this.mapListReverse(f.get()));
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        }).orElse(null);
+    }
 }
