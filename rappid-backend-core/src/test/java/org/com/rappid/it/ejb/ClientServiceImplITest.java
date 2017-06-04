1852b99dbf5b7bae36e77ef334c52b2010977349
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-04 17:54:45 -0500

    test(rappid-backend-core/ejb): add Arquillian IT @Deployment conf

--- rappid-backend-core/src/test/java/org/com/rappid/it/ejb/ClientServiceImplITest.java
+++ rappid-backend-core/src/test/java/org/com/rappid/it/ejb/ClientServiceImplITest.java
@@ -1,7 +1,15 @@
 package org.com.rappid.it.ejb;
 
-import org.apache.openejb.junit.jee.EJBContainerRunner;
 import org.com.rappid.api.ClientService;
+import org.com.rappid.ejb.ClientServiceImpl;
+import org.com.rappid.mapper.ClientMapper;
+import org.com.rappid.mapper.ClientMapperImpl;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,8 +19,15 @@ import javax.ejb.EJB;
 /**
  * Created by PINA on 22/05/2017.
  */
-@RunWith(EJBContainerRunner.class)
+@RunWith(Arquillian.class)
 public class ClientServiceImplITest {
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "test.jar")
+                .addClasses(ClientMapper.class, ClientMapperImpl.class, ClientServiceImpl.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
 
     @EJB
     private ClientService clientService;
