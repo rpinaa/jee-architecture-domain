299bc67cdca439891a19a8a7dc9095c01130e5ed
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-08 15:13:44 -0500

    feat(rappid-backend-core/mapper): add overloading map method to validate constraints from domain

--- rappid-backend-core/src/main/java/org/com/rappid/mapper/jpa/GenericMapper.java
+++ rappid-backend-core/src/main/java/org/com/rappid/mapper/jpa/GenericMapper.java
@@ -1,9 +1,13 @@
 package org.com.rappid.mapper.jpa;
 
 import javax.ejb.AsyncResult;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
@@ -13,9 +17,24 @@ import java.util.stream.Collectors;
 
 public interface GenericMapper<F, T extends Serializable> {
 
+    Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
     T map(final F from);
 
     F map(final T from);
+
+    default T map(final F from, final Class<?> ... groups) {
+
+        final T t = this.map(from);
+
+        final Set<ConstraintViolation<Object>> constraintsViolations = validator.validate(t, groups);
+
+        if (constraintsViolations.size() > 0) {
+            throw new RuntimeException(constraintsViolations.toString());
+        }
+
+        return t;
+    }
 
     default Future<F> map(final Future<T> from) {
         return Optional.of(from).map(f -> {
