0c6a1c89f3e3f20224a3b149ec936cc5a4b5f533
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-08 15:11:23 -0500

    feat(rappid-backend-core/group): add validation groups in each entity

--- rappid-backend-core/src/main/java/org/com/rappid/entity/ClientEntity.java
+++ rappid-backend-core/src/main/java/org/com/rappid/entity/ClientEntity.java
@@ -1,8 +1,11 @@
 package org.com.rappid.entity;
 
 import lombok.*;
+import org.com.rappid.group.client.CreateClientGroup;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by PINA on 28/05/2017.
@@ -26,20 +29,25 @@ public class ClientEntity extends AbstractEntity {
     @Column(name = "ID_CLIENT", length = 32, nullable = false, updatable = false)
     private String id;
 
+    @Valid
     @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "FK_ID_TELEPHONE")
     private TelephoneEntity telephone;
 
+    @NotNull(groups = {CreateClientGroup.class})
     @Column(name = "FIRST_NAME", length = 80)
     private String firstName;
 
+    @NotNull(groups = {CreateClientGroup.class})
     @Column(name = "LAST_NAME", length = 80)
     private String lastName;
 
+    @NotNull(groups = {CreateClientGroup.class})
     @Column(name = "EMAIL", length = 50)
     private String email;
 
     @Lob
+    @NotNull(groups = {CreateClientGroup.class})
     @Column(name = "SECRET")
     private byte[] secret;
 
---
+++ rappid-backend-core/src/main/java/org/com/rappid/group/client/CreateClientGroup.java
@@ -0,0 +1,7 @@
+package org.com.rappid.group.client;
+
+/**
+ * Created by PINA on 08/06/2017.
+ */
+public interface CreateClientGroup {
+}
