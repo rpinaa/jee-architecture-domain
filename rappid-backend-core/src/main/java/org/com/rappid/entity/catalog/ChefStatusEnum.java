18257a89d9358fb1632ae3d2465cd2e00c8c21a8
Ricardo Pina Arellano <ricardo.pina@bcm-corp.com>
2017-06-08 15:11:41 -0500

    feat(rappid-backend-core/entity): change enum name

--- rappid-backend-core/src/main/java/org/com/rappid/entity/catalog/ChefStatusEnum.java
+++ rappid-backend-core/src/main/java/org/com/rappid/entity/catalog/ChefStatusEnum.java
@@ -4,5 +4,5 @@ package org.com.rappid.entity.catalog;
  * Created by PINA on 31/05/2017.
  */
 public enum ChefStatusEnum {
-    PRE_REGISTERED, REGISTERED, ACTIVED, CANCELLED
+    PRE_REGISTERED, REGISTERED, ACTIVATED, CANCELLED
 }
